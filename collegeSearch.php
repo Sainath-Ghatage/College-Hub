36f719cb6d093cb3bb9496e18f66fbbd0d356913
Sainath Ghatage <sainathghatage18@gmail.com>
2025-03-24 22:31:48 +0530

    something is done

--- collegeSearch.php
+++ collegeSearch.php
@@ -7,27 +7,30 @@
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
     <link rel="stylesheet" href="css/style.css">
+    <style>
+          
+ </style>
 </head>
 <body class="bg-black text-white">
     <!--Search-->
     <!-- Search Box -->
-<div class="container bg-dark rounded text-warning p-1 mt-1">
+    <div class="container bg-dark rounded text-warning p-1 mt-1">
     <div class="container">
         <form method="GET" action="collegeSearch.php"> <!-- Add form -->
-            <div class="row">
+            <div class="row align-items-center"> <!-- Make the row a flex container -->
                 <div class="col-md-8">
                     <h2>Review Your College Anonymously</h2>
                 </div>
-                <div class="col-md-3 py-1">
-                    <input type="text" class="form-control" name="search" placeholder="Search for a college" value="<?php echo isset($_GET['search']) ? $_GET['search'] : ''; ?>">
-                </div>
-                <div class="col-md-1 pt-1">
+                <div class="col-md-4 d-flex justify-content-end align-items-center"> 
+                    <input type="text" class="form-control me-2" name="search" placeholder="Search for a college"
+                        value="<?php echo isset($_GET['search']) ? $_GET['search'] : ''; ?>">
                     <button class="btn btn-warning" type="submit">Search</button>
                 </div>
             </div>
         </form>
     </div>
 </div>
+
 
 <!--Reviews-->
     
@@ -56,7 +59,7 @@ if ($result->num_rows > 0) {
                     <div class='col-4'>
                         <img src='{$res['College_img']}' class='img-fluid rounded my-1' alt='College image'>
                     </div>
-                    <div class='col-8 bg-warning rounded my-1'>
+                    <div class='col-8 bg-warning rounded my-1 mr-1  '>
                         <div class='p-3'>
                             <h4 class='text-black'>{$res['College_Name']}</h4>
                             <h5 class='text-dark'>{$res['College_Address']}</h5>
@@ -76,6 +79,12 @@ if ($result->num_rows > 0) {
 
 $conn->close();
 ?>
+
+<footer class="container-fluid text-center mt-5">
+        <p><a href="#">Back to top</a></p>
+        <p>&copy; <script>document.write(new Date().getFullYear());</script> College Hub</p>
+       
+    </footer>
     <script src="js/bootstrap.bundle.js"></script>
 </body>
 </html>
